e9ab3930ec15c649c226e65768d9caa915aa61df
ngovuong-dev <ngovuong.dev@gmail.com>
2026-01-27 18:13:38 +0700

    chore: Update config setup arch

---
+++ config/nvim/lua/plugins/theme.lua
@@ -0,0 +1,37 @@
+return {
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		opts = {
+			transparent_background = true, -- Bật trong suốt cho Catppuccin
+			integrations = {
+				neotree = true,
+				mason = true,
+				native_lsp = {
+					enabled = true,
+					virtual_text = {
+						errors = { "italic" },
+						hints = { "italic" },
+						warnings = { "italic" },
+						information = { "italic" },
+					},
+					underlines = {
+						errors = { "underline" },
+						hints = { "underline" },
+						warnings = { "underline" },
+						information = { "underline" },
+					},
+					inlay_hints = {
+						background = true,
+					},
+				},
+			},
+		},
+	},
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "catppuccin-mocha", -- Chọn tông màu (mocha, macchiato, frappe, latte)
+		},
+	},
+}
